179088f5d240b76caccd0b897591a65c8c13b4b7
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-01-08 19:55:32 +0530

    added react router

--- art-gallery/src/App.jsx
+++ art-gallery/src/App.jsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router";
+import LoginPage from "./pages/Auth/LoginPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   )
 }
