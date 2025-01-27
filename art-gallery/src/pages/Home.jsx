cc1989ba67065bb97b5e651f838c63c212d70e70
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-01-28 00:08:48 +0530

    home page updated

---
+++ art-gallery/src/pages/Home.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import GridMotion from '../components/GridMotion';
+import BlurText from "../components/BlurText";
+
+const Home = () => {
+
+const items = [
+  'Item 1',
+  <div key='jsx-item-1'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 2',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 4',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 5',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 7',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 8',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 10',
+  <div key='jsx-item-3'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 11',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 13',
+  <div key='jsx-item-4'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 14',
+  'Item 1',
+  <div key='jsx-item-1'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 2',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 4',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 5',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 7',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 8',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 10',
+  <div key='jsx-item-3'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 11',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 13',
+  <div key='jsx-item-4'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 14',
+  // Add more items as needed
+];
+  return (
+    <div className="relative h-screen bg-gray-200 flex justify-center items-center overflow-hidden">
+  {/* GridMotion as the background */}
+  <div className="absolute inset-0">
+    <div className="relative h-full w-full">
+      <GridMotion items={items} />
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-50 pointer-events-none"></div>
+    </div>
+  </div>
+
+  {/* Header in the center, in the foreground */}
+  <BlurText
+  text="Welcome to Art Gallery!!!"
+  delay={150}
+  animateBy="words"
+  direction="top"
+  className="relative z-10 text-5xl mb-8 text-white font-bold"
+/>
+</div>
+
+  )
+}
+
+export default Home
+
+
